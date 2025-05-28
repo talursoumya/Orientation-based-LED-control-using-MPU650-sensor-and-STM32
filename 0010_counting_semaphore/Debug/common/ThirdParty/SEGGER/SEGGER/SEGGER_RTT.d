fa54d91a925e5070952786496ac154124bd7494c
Soumya M Talur <83412397+talursoumya@users.noreply.github.com>
2025-05-28 11:08:45 +0530

    Delete 0010_counting_semaphore directory

--- 0010_counting_semaphore/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.o: \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.c \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
