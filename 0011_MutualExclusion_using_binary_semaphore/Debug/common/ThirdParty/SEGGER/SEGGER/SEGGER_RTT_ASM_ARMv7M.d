1bc0c05f29f97013da95b5c15bc2bb1d2f6759b7
Soumya M Talur <83412397+talursoumya@users.noreply.github.com>
2025-05-28 11:09:20 +0530

    Delete 0011_MutualExclusion_using_binary_semaphore directory

--- 0011_MutualExclusion_using_binary_semaphore/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
