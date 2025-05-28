c4bdc61083ab71df09378f7ecfcd721992842f8e
Soumya M Talur <83412397+talursoumya@users.noreply.github.com>
2025-05-28 11:11:45 +0530

    Delete 007_Task_priority_exchange directory

--- 007_Task_priority_exchange/Debug/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.o: \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_printf.c \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
