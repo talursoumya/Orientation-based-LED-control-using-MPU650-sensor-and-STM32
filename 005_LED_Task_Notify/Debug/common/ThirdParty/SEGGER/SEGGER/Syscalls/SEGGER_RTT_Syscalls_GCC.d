b322430edfaf7b6cb59eb9745a3467c60b1b9f0e
Soumya M Talur <83412397+talursoumya@users.noreply.github.com>
2025-05-28 11:11:12 +0530

    Delete 005_LED_Task_Notify directory

--- 005_LED_Task_Notify/Debug/common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.d
+++
@@ -1,6 +0,0 @@
-common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.o: \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/Syscalls/SEGGER_RTT_Syscalls_GCC.c \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/STM32F407G_Development/Development/RTOS/common/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
