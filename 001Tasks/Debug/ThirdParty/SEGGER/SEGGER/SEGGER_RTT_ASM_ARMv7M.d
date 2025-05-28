26f5cee399655a8ae67fab6dfc563d94a51da77f
Soumya M Talur <83412397+talursoumya@users.noreply.github.com>
2025-05-28 11:09:34 +0530

    Delete 001Tasks directory

--- 001Tasks/Debug/ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.d
+++
@@ -1,6 +0,0 @@
-ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.o: \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT_ASM_ARMv7M.S \
- ../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h \
- C:/STM32F407G_Development/Development/RTOS/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h
-../ThirdParty/SEGGER/SEGGER/SEGGER_RTT.h:
-C:/STM32F407G_Development/Development/RTOS/001Tasks/ThirdParty/SEGGER/Config/SEGGER_RTT_Conf.h:
